0b115174abf4db0abc2c02c8daf0403969d93201
ddorokhov <denis.dorokhov@insparx.com>
2017-06-11 13:22:07 +0200

    make advice a part of the controller

--- src/main/java/net/dorokhov/pony/web/controller/AuthenticationController.java
+++ src/main/java/net/dorokhov/pony/web/controller/AuthenticationController.java
@@ -6,8 +6,12 @@ import net.dorokhov.pony.user.service.UserService;
 import net.dorokhov.pony.user.service.exception.InvalidCredentialsException;
 import net.dorokhov.pony.user.service.exception.NotAuthenticatedException;
 import net.dorokhov.pony.web.domain.CredentialsDto;
+import net.dorokhov.pony.web.domain.ErrorDto;
 import net.dorokhov.pony.web.domain.UserDto;
 import net.dorokhov.pony.web.domain.UserTokenDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,6 +19,27 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/authentication")
 public class AuthenticationController implements ResponseBodyController {
+
+    @ControllerAdvice(assignableTypes = AuthenticationController.class)
+    @ResponseBody
+    public static class Advice {
+
+        private final Logger logger = LoggerFactory.getLogger(getClass());
+
+        @ExceptionHandler(InvalidCredentialsException.class)
+        @ResponseStatus(HttpStatus.UNAUTHORIZED)
+        public ErrorDto onInvalidCredentials(InvalidCredentialsException e) {
+            logger.debug("Credentials are invalid.");
+            return new ErrorDto(ErrorDto.Code.INVALID_CREDENTIALS, "Credentials are invalid.");
+        }
+
+        @ExceptionHandler(NotAuthenticatedException.class)
+        @ResponseStatus(HttpStatus.UNAUTHORIZED)
+        public ErrorDto onNotAuthenticated(NotAuthenticatedException e) {
+            logger.debug("User is not authenticated.");
+            return new ErrorDto(ErrorDto.Code.ACCESS_DENIED, "Access denied.");
+        }
+    }
     
     private final UserService userService;
     private final CurrentUserService currentUserService;
--- src/main/java/net/dorokhov/pony/web/controller/InstallationController.java
+++ src/main/java/net/dorokhov/pony/web/controller/InstallationController.java
@@ -1,19 +1,34 @@
 package net.dorokhov.pony.web.controller;
 
 import net.dorokhov.pony.installation.service.exception.AlreadyInstalledException;
+import net.dorokhov.pony.web.domain.ErrorDto;
 import net.dorokhov.pony.web.domain.InstallationCommandDto;
 import net.dorokhov.pony.web.domain.InstallationDto;
 import net.dorokhov.pony.web.service.InstallationServiceFacade;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/installation")
 public class InstallationController implements ResponseBodyController {
+
+    @ControllerAdvice(assignableTypes = InstallationController.class)
+    @ResponseBody
+    public static class Advice {
+
+        private final Logger logger = LoggerFactory.getLogger(getClass());
+
+        @ExceptionHandler(AlreadyInstalledException.class)
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        public ErrorDto onAlreadyInstalled() {
+            logger.warn("Application is already installed.");
+            return new ErrorDto(ErrorDto.Code.BAD_REQUEST, "Bad request.");
+        }
+    }
     
     private final InstallationServiceFacade installationServiceFacade;
 
