435c91f9b6cbdb21434726033cc2fa42cf50d1d5
ddorokhov <denis.dorokhov@insparx.com>
2017-12-26 11:09:52 +0100

    add security api to swagger

---
+++ backend/web/src/main/java/net/dorokhov/pony/web/security/SecurityApiListingScanner.java
@@ -0,0 +1,69 @@
+package net.dorokhov.pony.web.security;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.classmate.TypeResolver;
+import com.google.common.collect.ImmutableSet;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.OperationBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiDescription;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.ApiListingScannerPlugin;
+import springfox.documentation.spi.service.contexts.DocumentationContext;
+import springfox.documentation.spring.web.readers.operation.CachingOperationNameGenerator;
+
+import static java.util.Collections.singletonList;
+
+@Component
+public class SecurityApiListingScanner implements ApiListingScannerPlugin {
+
+    @Override
+    public List<ApiDescription> apply(DocumentationContext context) {
+        return new ArrayList<>(singletonList(
+                new ApiDescription(
+                        "/api/authentication",
+                        "Security operations.",
+                        Arrays.asList(
+                                new OperationBuilder(new CachingOperationNameGenerator())
+                                        .tags(ImmutableSet.of("security"))
+                                        .codegenMethodNameStem("apiAuthenticationPOST")
+                                        .method(HttpMethod.POST)
+                                        .summary("Log user in.")
+                                        .parameters(Arrays.asList(
+                                                new ParameterBuilder()
+                                                        .description("email")
+                                                        .type(new TypeResolver().resolve(String.class))
+                                                        .modelRef(new ModelRef("string"))
+                                                        .name("email")
+                                                        .parameterType("form")
+                                                        .build(),
+                                                new ParameterBuilder()
+                                                        .description("command")
+                                                        .type(new TypeResolver().resolve(String.class))
+                                                        .modelRef(new ModelRef("string"))
+                                                        .name("password")
+                                                        .parameterType("form")
+                                                        .build()
+                                        ))
+                                        .build(),
+                                new OperationBuilder(new CachingOperationNameGenerator())
+                                        .tags(ImmutableSet.of("security"))
+                                        .codegenMethodNameStem("apiAuthenticationDELETE")
+                                        .method(HttpMethod.DELETE)
+                                        .summary("Log user out.")
+                                        .build()
+                        ), 
+                        false)
+        ));
+    }
+
+    @Override
+    public boolean supports(DocumentationType delimiter) {
+        return DocumentationType.SWAGGER_2.equals(delimiter);
+    }
+}
