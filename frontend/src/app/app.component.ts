7a9bae3fe368bcc53ad7822d3a9c635415f4290f
ddorokhov <denis.dorokhov@insparx.com>
2018-01-05 21:23:40 +0100

    upgrade angular to v5

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pony-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'pony';
 }
