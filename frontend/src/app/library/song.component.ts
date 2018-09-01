63f48804e9bf2868aa8bfd8a9b6c9ffd53973e1f
ddorokhov <denis.dorokhov@insparx.com>
2018-09-01 09:51:21 +0200

    show playback status in song view

--- frontend/src/app/library/song.component.ts
+++ frontend/src/app/library/song.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs/Subscription';
 import {Song} from '../core/library/library.model';
 import {LibraryService} from '../core/library/library.service';
+import {PlaybackService, PlaybackState} from '../core/library/playback.service';
 
 @Component({
   selector: 'pony-song',
@@ -9,16 +10,23 @@ import {LibraryService} from '../core/library/library.service';
   styleUrls: ['./song.component.scss']
 })
 export class SongComponent implements OnInit, OnDestroy {
+  
+  PlaybackState = PlaybackState;
 
   @Input() song: Song;
   @Input() showArtist = false;
 
   selected = false;
   duration: string;
+  playbackState: PlaybackState | undefined;
 
   private selectedSongSubscription: Subscription;
+  private playbackEventSubscription: Subscription;
 
-  constructor(private libraryService: LibraryService) {
+  constructor(
+    private libraryService: LibraryService,
+    private playbackService: PlaybackService
+  ) {
   }
 
   ngOnInit(): void {
@@ -26,11 +34,20 @@ export class SongComponent implements OnInit, OnDestroy {
       .subscribe(song => {
         this.selected = song && song.id === this.song.id;
       });
+    this.playbackEventSubscription = this.playbackService.observePlaybackEvent()
+      .subscribe(playbackEvent => {
+        if (playbackEvent.song && playbackEvent.song.id === this.song.id) {
+          this.playbackState = playbackEvent.state;
+        } else {
+          this.playbackState = undefined;
+        }
+      });
     this.duration = this.song.durationInMinutes;
   }
 
   ngOnDestroy(): void {
     this.selectedSongSubscription.unsubscribe();
+    this.playbackEventSubscription.unsubscribe();
   }
 
   select() {
