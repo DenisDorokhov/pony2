236d96a8206f5f840c7b046ef35cad2d2120fac7
ddorokhov <denis.dorokhov@insparx.com>
2018-10-15 22:08:42 +0300

    fix caching of static files

--- backend/web/src/main/java/net/dorokhov/pony/web/WebConfig.java
+++ backend/web/src/main/java/net/dorokhov/pony/web/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -92,6 +93,11 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
                 .securitySchemes(newArrayList(
                         new ApiKey("token", "Authorization", "header")
                 ));
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/*", "/assets/**");
     }
 
     @Override
