15597c49b45499cb31a714d4e89dbdc5c6649111
DenisDorokhov <denis@dorokhov.net>
2024-07-12 21:50:09 +0200

    init

---
+++ src/main/java/net/dorokhov/pony3/app/PonyApplication.java
@@ -0,0 +1,13 @@
+package net.dorokhov.pony3.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dorokhov/pony3/app/PonyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dorokhov.pony3.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
