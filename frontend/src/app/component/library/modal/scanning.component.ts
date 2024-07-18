9fa1beb83ddb425f24a21f3c549785bec54a98a5
Denis Dorokhov <denis@dorokhov.net>
2024-07-18 22:16:41 +0200

    scanning modal prototype

--- frontend/src/app/component/library/modal/scanning.component.ts
+++ frontend/src/app/component/library/modal/scanning.component.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {TranslateModule} from "@ngx-translate/core";
+import {LoadingIndicatorComponent} from "../../common/loading-indicator.component";
+import {ErrorIndicatorComponent} from "../../common/error-indicator.component";
+import {NoContentIndicatorComponent} from "../../common/no-content-indicator.component";
 
 @Component({
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, LoadingIndicatorComponent, ErrorIndicatorComponent, NoContentIndicatorComponent],
   selector: 'pony-scanning',
   templateUrl: './scanning.component.html',
   styleUrls: ['./scanning.component.scss']
