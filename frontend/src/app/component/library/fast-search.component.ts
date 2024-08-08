cf8a28714ccfd3af20d8d86b2c18501f9b2f30ba
Denis Dorokhov <denis@dorokhov.net>
2024-08-08 19:44:31 +0200

    search keyboard navigation

--- frontend/src/app/component/library/fast-search.component.ts
+++ frontend/src/app/component/library/fast-search.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from "@angular/core";
+import {Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren} from "@angular/core";
 import {TranslateModule} from "@ngx-translate/core";
 import {CommonModule} from "@angular/common";
 import {debounceTime, mergeMap, of, Subject, Subscription} from "rxjs";
@@ -6,6 +6,20 @@ import {distinctUntilChanged, map} from "rxjs/operators";
 import {LibraryService} from "../../service/library.service";
 import {Album, Artist, SearchResult, Song} from "../../domain/library.model";
 import {ImageLoaderComponent} from "../common/image-loader.component";
+import {ScrollingUtils} from "../../utils/scrolling.utils";
+import scrollIntoElement = ScrollingUtils.scrollIntoElement;
+
+class NavigationItem {
+
+  id: string;
+  selected = false;
+  activate: () => void;
+
+  constructor(id: string, activate: () => void) {
+    this.id = id;
+    this.activate = activate;
+  }
+}
 
 @Component({
   standalone: true,
@@ -19,8 +33,13 @@ export class FastSearchComponent implements OnInit, OnDestroy {
   open = false;
   searchResult: SearchResult | undefined;
 
+  navigationItems: NavigationItem[] = [];
+  idToNavigationItem: {[key: string]: NavigationItem} = {};
+
   @ViewChild('container') containerElement!: ElementRef;
+  @ViewChild('input') inputElement!: ElementRef;
   @ViewChild('searchResults') searchResultsElement!: ElementRef;
+  @ViewChildren('links') linkElements!: QueryList<ElementRef>;
 
   private searchSubject = new Subject<string>();
 
@@ -37,6 +56,7 @@ export class FastSearchComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.searchSubscription = this.searchSubject.pipe(
+      map(value => value.trim()),
       debounceTime(200),
       distinctUntilChanged(),
       mergeMap(query => {
@@ -55,6 +75,23 @@ export class FastSearchComponent implements OnInit, OnDestroy {
       })
     ).subscribe(searchResult => {
       this.searchResult = searchResult;
+      this.navigationItems = [];
+      this.idToNavigationItem = {};
+      this.searchResult?.artists.forEach(artist => {
+        const navigationItem = new NavigationItem(artist.id, () => this.selectArtist(artist));
+        this.navigationItems.push(navigationItem);
+        this.idToNavigationItem[navigationItem.id] = navigationItem;
+      });
+      this.searchResult?.songs.forEach(song => {
+        const navigationItem = new NavigationItem(song.id, () => this.selectSong(song));
+        this.navigationItems.push(navigationItem);
+        this.idToNavigationItem[navigationItem.id] = navigationItem;
+      });
+      this.searchResult?.albums.forEach(album => {
+        const navigationItem = new NavigationItem(album.id, () => this.selectAlbum(album));
+        this.navigationItems.push(navigationItem);
+        this.idToNavigationItem[navigationItem.id] = navigationItem;
+      });
       this.searchResultsElement.nativeElement.scrollTop = 0;
     });
     window.document.body.addEventListener('mousedown', event => {
@@ -78,22 +115,71 @@ export class FastSearchComponent implements OnInit, OnDestroy {
     this.open = true;
   }
 
-  onSongSelection(song: Song) {
+  onInputKeyDown(event: KeyboardEvent) {
+    if (event.key === 'ArrowDown') {
+      this.moveNavigation(true);
+      event.preventDefault();
+    }
+    if (event.key === 'ArrowUp') {
+      this.moveNavigation(false);
+      event.preventDefault();
+    }
+    if (event.key === 'Enter') {
+      const selectedIndex = this.indexOfSelectedNavigationItem();
+      if (selectedIndex !== undefined) {
+        this.navigationItems[selectedIndex].activate();
+      }
+      event.preventDefault();
+    }
+    if (event.key === 'Escape') {
+      this.open = false;
+      this.inputElement.nativeElement.blur();
+      event.preventDefault();
+    }
+  }
+
+  private indexOfSelectedNavigationItem() {
+    return this.navigationItems.findIndex(next => next.selected);
+  }
+
+  private moveNavigation(next: boolean) {
+    const selectedIndex = this.indexOfSelectedNavigationItem();
+    this.navigationItems.forEach(next => next.selected = false);
+    let indexToSelect: number | undefined = next ? selectedIndex + 1 : selectedIndex - 1;
+    if (indexToSelect < 0) {
+      indexToSelect = this.navigationItems.length - 1;
+    }
+    if (indexToSelect > this.navigationItems.length - 1) {
+      indexToSelect = 0;
+    }
+    if (indexToSelect >= 0) {
+      this.navigationItems[indexToSelect].selected = true;
+      scrollIntoElement(this.linkElements.toArray()[indexToSelect].nativeElement, false);
+    }
+  }
+
+  selectSong(song: Song, close = true) {
     this.libraryService.selectArtistAndMakeDefault(song.album.artist);
     this.libraryService.selectSong(song);
     this.libraryService.startScrollToSong(song);
-    this.open = false;
+    if (close) {
+      this.open = false;
+    }
   }
 
-  onAlbumSelection(album: Album) {
+  selectAlbum(album: Album, close = true) {
     this.libraryService.selectArtistAndMakeDefault(album.artist);
     this.libraryService.startScrollToAlbum(album);
-    this.open = false;
+    if (close) {
+      this.open = false;
+    }
   }
 
-  onArtistSelection(artist: Artist) {
+  selectArtist(artist: Artist, close = true) {
     this.libraryService.selectArtistAndMakeDefault(artist);
     this.libraryService.startScrollToArtist(artist);
-    this.open = false;
+    if (close) {
+      this.open = false;
+    }
   }
 }
