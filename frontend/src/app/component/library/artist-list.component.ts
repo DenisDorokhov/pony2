0c7e62cb00d508d4323a92c5753d07d70623e9f9
Denis Dorokhov <denis@dorokhov.net>
2025-01-05 00:21:01 +0100

    artist genres fixes

--- frontend/src/app/component/library/artist-list.component.ts
+++ frontend/src/app/component/library/artist-list.component.ts
@@ -118,7 +118,7 @@ export class ArtistListComponent implements OnInit, OnDestroy {
       this.scrollerElement.nativeElement.scrollTop = 0;
     }
     if (this.filteredArtists.findIndex(artist => artist.id === this.libraryService.selectedArtist?.id) > -1) {
-      this.libraryService.requestScrollToArtist(this.libraryService.selectedArtist!);
+      requestAnimationFrame(() => this.libraryService.requestScrollToArtist(this.libraryService.selectedArtist!));
     }
   }
 }
