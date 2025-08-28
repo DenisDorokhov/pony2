a9549e799e9c4abd4ba7bdff4ad2f6b177789549
Denis Dorokhov <denis@dorokhov.net>
2025-08-29 00:12:41 +0200

    open subsonic api support - playlist cover art

--- backend/src/main/java/net/dorokhov/pony2/web/controller/opensubsonic/OpenSubsonicFileController.java
+++ backend/src/main/java/net/dorokhov/pony2/web/controller/opensubsonic/OpenSubsonicFileController.java
@@ -2,12 +2,19 @@ package net.dorokhov.pony2.web.controller.opensubsonic;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import net.dorokhov.pony2.web.dto.PlaylistSongsDto;
 import net.dorokhov.pony2.web.service.FileDistributor;
 import net.dorokhov.pony2.web.service.FileFacade;
+import net.dorokhov.pony2.web.service.PlaylistFacade;
+import net.dorokhov.pony2.web.service.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import static net.dorokhov.pony2.web.common.StreamingUtils.handleStreamingExceptions;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -20,10 +27,16 @@ public class OpenSubsonicFileController implements OpenSubsonicController {
 
     private final FileFacade fileFacade;
     private final FileDistributor fileDistributor;
+    private final PlaylistFacade playlistFacade;
 
-    public OpenSubsonicFileController(FileFacade fileFacade, FileDistributor fileDistributor) {
+    public OpenSubsonicFileController(
+            FileFacade fileFacade,
+            FileDistributor fileDistributor,
+            PlaylistFacade playlistFacade
+    ) {
         this.fileFacade = fileFacade;
         this.fileDistributor = fileDistributor;
+        this.playlistFacade = playlistFacade;
     }
 
     @RequestMapping(value = {
@@ -35,7 +48,34 @@ public class OpenSubsonicFileController implements OpenSubsonicController {
     }
 
     @RequestMapping(value = "/opensubsonic/rest/getCoverArt.view", produces = {"image/*", APPLICATION_JSON_VALUE}, method = {GET, POST})
-    public void getCoverArt(@RequestParam("id") String artworkId, HttpServletRequest request, HttpServletResponse response) {
-        handleStreamingExceptions(() -> fileDistributor.distribute(fileFacade.getLargeArtworkDistribution(artworkId), request, response), response);
+    public void getCoverArt(@RequestParam("id") String artworkOrPlaylistId, HttpServletRequest request, HttpServletResponse response) {
+        String resolvedArtworkId;
+        if (artworkOrPlaylistId.startsWith("pl-")) {
+            resolvedArtworkId = resolvePlaylistArtworkId(artworkOrPlaylistId.replaceAll("^pl-", "")).orElse(artworkOrPlaylistId);
+        } else {
+            resolvedArtworkId = artworkOrPlaylistId;
+        }
+        handleStreamingExceptions(() -> fileDistributor.distribute(fileFacade.getLargeArtworkDistribution(resolvedArtworkId), request, response), response);
+    }
+
+    private Optional<String> resolvePlaylistArtworkId(String playlistId) {
+        try {
+            PlaylistSongsDto playlist = playlistFacade.getPlaylistById(playlistId);
+            return resolvePlaylistArtworkId(playlist);
+        } catch (ObjectNotFoundException e) {
+            return Optional.empty();
+        }
+    }
+
+    private Optional<String> resolvePlaylistArtworkId(PlaylistSongsDto playlist) {
+        List<String> artworks = playlist.getSongs().stream()
+                .map(playlistSong -> playlistSong.getSong().getAlbumDetails().getAlbum().getArtworkId())
+                .filter(Objects::nonNull)
+                .toList();
+        if (artworks.isEmpty()) {
+            return Optional.empty();
+        }
+        int artworkIndex = (int) Math.floor(artworks.size() / 2.0);
+        return Optional.of(artworks.get(artworkIndex));
     }
 }
