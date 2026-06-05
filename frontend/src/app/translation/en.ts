41d97f30b3dbf3fb6e743ba15e09faf2109ea408
Denis Dorokhov <denis@dorokhov.net>
2026-06-05 23:04:41 +0200

    fix regression bugs

--- frontend/src/app/translation/en.ts
+++ frontend/src/app/translation/en.ts
@@ -61,8 +61,8 @@ export default {
       addToPlaylist: 'Add to "{name}"',
     },
     artist: {
-      allArtistsLabel: 'All Genres ({artistCount})',
-      allArtistsTitleLabel: 'All Genres',
+      allArtistsLabel: 'All Artists ({artistCount})',
+      allArtistsTitleLabel: 'All Artists',
       updatedArtistsLabel: 'Recently Updated ({artistCount})',
       updatedArtistsTitleLabel: 'Recently Updated',
       unknownLabel: 'Unknown Artist',
